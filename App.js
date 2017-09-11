c698978c96dda67180495a92e04049a816bd3062
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-09-10 22:42:49 -0700

    Core algo v1

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CryptoJS from 'crypto-js';
 
 export default class App extends React.Component {
   render() {
+    const secret = CryptoJS.SHA256("mypassword:yopa.io:0").toString(CryptoJS.enc.Base64).substring(0, 16);
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>YoPa!</Text>
+        <Text>secret("mypassword:yopa.io:0") =</Text>
+        <Text>{secret}</Text>
       </View>
     );
   }
