b4f8680a3e00b62eef4ee838c97bd85b123db6f7
Davide Vernizzi <davide.vernizzi@etiqa.it>
2017-09-14 23:01:16 +0200

    Basic tabbar layout

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TabBarIOS } from 'react-native';
 import { Header } from './Header';
+import { List } from './List';
 import { InputBar } from './InputBar';
 import { YoPass } from './YoPa';
 import { styles } from '../styles/Main';
@@ -9,15 +10,42 @@ import { styles } from '../styles/Main';
 export class Main extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
+      <TabBarIOS selectedTab={this.props.state.selectedTab}>
+        <TabBarIOS.Item
+          selected={this.props.state.selectedTab === 'List'}
+          title={'List'}
+          onPress={() => {
+              this.props.updateState({
+                  selectedTab: 'List',
+              });
+          }}>
+            <View style={styles.container}>
+              <Header />
+              <List />
+            </View>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          selected={this.props.state.selectedTab === 'Add'}
+          title={'Add'}
+          onPress={() => {
+              this.props.updateState({
+                  selectedTab: 'Add',
+              });
+          }}>
+            <View style={styles.container}>
+              <Header />
 
-        <InputBar updateState={this.props.updateState} />
+              <View style={styles.body}>
+                <InputBar updateState={this.props.updateState} />
 
-        <YoPass pass={this.props.state.password}
-                site={this.props.state.site}
-                counter={this.props.state.counter} />
-      </View>
+                <YoPass pass={this.props.state.password}
+                        site={this.props.state.site}
+                        counter={this.props.state.counter} />
+              </View>
+            </View>
+        </TabBarIOS.Item>
+
+      </TabBarIOS>
     );
   }
 }
