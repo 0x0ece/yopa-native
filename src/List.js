698d29d0df05d46bcb98aa002df74f98ac08d7a6
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-09-15 19:31:29 -0700

    Experiment with DocumentPicker and FileSystem

--- src/List.js
+++ src/List.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import {styles} from '../styles/Main';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { DocumentPicker, FileSystem } from 'expo';
+import yaml from 'js-yaml';
+
+import { styles } from '../styles/Main';
 
 
 export class List extends React.Component {
+
+
+  onClick() {
+    DocumentPicker.getDocumentAsync({ 'type': 'text/*' })
+      .then(result => {
+        if (result.type == 'success') {
+          const fileSrc = result.uri;
+          const fileDst = FileSystem.documentDirectory + 'data.yml';
+          FileSystem.downloadAsync(fileSrc, fileDst)
+          .then(({ uri }) => {
+            FileSystem.readAsStringAsync(fileDst).then(txt => {
+              const data = yaml.safeLoad(txt);
+              console.log(data);
+            });
+          })
+          .catch(error => {
+            console.error(error);
+          });
+        }
+      });
+  }
+
   render() {
     return(
       <View style={styles.body}>
         <Text>List of passwords</Text>
+        <Button
+          onPress={this.onClick}
+          title="Tap me"
+        />
       </View>
     )
   }
