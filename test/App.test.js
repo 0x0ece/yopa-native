4d6d0f2e7ca81dc285154e2ca546c59332f63757
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-10-22 20:29:56 -0700

    Fix tests

--- test/App.test.js
+++ test/App.test.js
@@ -1,20 +1,20 @@
 import 'react-native';
-// import React from 'react';
-// import renderer from 'react-test-renderer';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { View } from 'react-native';
 
-// import App from '../App';
+import App from '../App';
 
 
-// TODO: test view
+jest.mock('../src/components/SwipeableRow', () => 'SwipeableRow');
 
 it('does nothing', () => {
   expect(1).toBe(1);
 });
 
-// it('renders correctly', () => {
-//   const component = renderer.create(
-//     <App />,
-//   );
-//   let tree = component.toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders correctly', () => {
+  const component = renderer.create(
+    <App />,
+  );
+  expect(component).toBeTruthy();
+});
