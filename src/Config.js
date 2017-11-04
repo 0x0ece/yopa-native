0c4879b538a968140ad1949b707eaefbbdce3a04
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-11-04 11:44:20 -0700

    Add service description when user saves it. Fix #82

--- src/Config.js
+++ src/Config.js
@@ -6,6 +6,11 @@ const Config = {
 
   AMPLITUDE_API_KEY: 'cbafe8abda90290beea53eb177ab25f2',
 
+  SERVICE_AUTO_DESCRIPTION: {
+    google: 'gmail gplus gdocs',
+    facebook: 'messenger',
+  },
+
   Android: (Platform.OS === 'android'),
   DeviceSecurity: undefined,
 
