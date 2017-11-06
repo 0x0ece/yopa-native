e5026c18fb708690e51f8e8e344ab23add827407
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-11-05 23:56:00 -0800

    Service screen: copy user/pass, go to site. Fix #68

--- src/screens/ServiceScreen.js
+++ src/screens/ServiceScreen.js
@@ -2,23 +2,130 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import t from 'tcomb-form-native';
-import { ActionSheetIOS, ScrollView, Alert, Clipboard, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import {
+  ActionSheetIOS,
+  Alert,
+  Clipboard,
+  Linking,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
+import RectButton from '../components/RectButton';
 import Style from '../Style';
+import { Service } from '../Models';
 import { updateService, delService } from '../redux/actions';
 
 
-function getStateFromProps(props) {
-  const service = props.service;
-  const group = props.group;
+function callbackTemplate(locals, iconName, onPressCallback) {
+  if (locals.hidden) {
+    return null;
+  }
 
-  return { value: {
-    service: service.service,
-    username: service.username,
-    secret: service.getSecret(group),
-  },
-  };
+  const stylesheet = locals.stylesheet;
+  let formGroupStyle = stylesheet.formGroup.normal;
+  let controlLabelStyle = stylesheet.controlLabel.normal;
+  let textboxStyle = stylesheet.textbox.normal;
+  let textboxViewStyle = stylesheet.textboxView.normal;
+  let helpBlockStyle = stylesheet.helpBlock.normal;
+  const errorBlockStyle = stylesheet.errorBlock;
+
+  if (locals.hasError) {
+    formGroupStyle = stylesheet.formGroup.error;
+    controlLabelStyle = stylesheet.controlLabel.error;
+    textboxStyle = stylesheet.textbox.error;
+    textboxViewStyle = stylesheet.textboxView.error;
+    helpBlockStyle = stylesheet.helpBlock.error;
+  }
+
+  if (locals.editable === false) {
+    textboxStyle = stylesheet.textbox.notEditable;
+    textboxViewStyle = stylesheet.textboxView.notEditable;
+  }
+
+  const label = locals.label ? (
+    <Text style={controlLabelStyle}>{locals.label}</Text>
+  ) : null;
+  const help = locals.help ? (
+    <Text style={helpBlockStyle}>{locals.help}</Text>
+  ) : null;
+  const error =
+    locals.hasError && locals.error ? (
+      <Text accessibilityLiveRegion="polite" style={errorBlockStyle}>
+        {locals.error}
+      </Text>
+    ) : null;
+
+  return (
+    <View style={formGroupStyle}>
+      {label}
+      <RectButton
+        onPress={() => onPressCallback(locals.value)}
+        style={[textboxViewStyle,
+          {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: textboxStyle.marginBottom,
+          }]}
+      >
+        <TextInput
+          accessibilityLabel={locals.label}
+          autoCapitalize={locals.autoCapitalize}
+          autoCorrect={locals.autoCorrect}
+          autoFocus={locals.autoFocus}
+          blurOnSubmit={locals.blurOnSubmit}
+          editable={locals.editable}
+          keyboardType={locals.keyboardType}
+          maxLength={locals.maxLength}
+          multiline={locals.multiline}
+          onBlur={locals.onBlur}
+          onEndEditing={locals.onEndEditing}
+          onFocus={locals.onFocus}
+          onLayout={locals.onLayout}
+          onSelectionChange={locals.onSelectionChange}
+          onSubmitEditing={locals.onSubmitEditing}
+          onContentSizeChange={locals.onContentSizeChange}
+          placeholderTextColor={locals.placeholderTextColor}
+          secureTextEntry={locals.secureTextEntry}
+          selectTextOnFocus={locals.selectTextOnFocus}
+          selectionColor={locals.selectionColor}
+          numberOfLines={locals.numberOfLines}
+          underlineColorAndroid={locals.underlineColorAndroid}
+          clearButtonMode={locals.clearButtonMode}
+          clearTextOnFocus={locals.clearTextOnFocus}
+          enablesReturnKeyAutomatically={locals.enablesReturnKeyAutomatically}
+          keyboardAppearance={locals.keyboardAppearance}
+          onKeyPress={locals.onKeyPress}
+          returnKeyType={locals.returnKeyType}
+          selectionState={locals.selectionState}
+          onChangeText={value => locals.onChange(value)}
+          onChange={locals.onChangeNative}
+          placeholder={locals.placeholder}
+          style={[textboxStyle, { flex: 1, marginBottom: 0 }]}
+          value={locals.value}
+        />
+        <Icon name={iconName} style={{ width: 32, alignItems: 'center' }} />
+      </RectButton>
+      {help}
+      {error}
+    </View>
+  );
+}
+
+function copyTemplate(locals) {
+  return callbackTemplate(locals, 'content-copy', (value) => {
+    Clipboard.setString(value);
+  });
+}
+
+function linkTemplate(locals) {
+  return callbackTemplate(locals, 'link', (value) => {
+    Linking.openURL(`https://${value}`);
+  });
 }
 
 
@@ -26,45 +133,48 @@ const Form = t.form.Form;
 class ServiceScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = getStateFromProps(this.props.navigation.state.params);
+
+    const params = props.navigation.state.params;
+    const service = params.service;
+    const group = params.group;
+    this.state = {
+      value: {
+        service: service.service,
+        username: service.username,
+        secret: service.getSecret(group),
+      },
+    };
 
     this.handlePress = this.handlePress.bind(this);
-    this.generateNewSecret = this.generateNewSecret.bind(this);
-    this.revertSecret = this.revertSecret.bind(this);
+    this.updateSecret = this.updateSecret.bind(this);
     this.deleteService = this.deleteService.bind(this);
-    // this.onChange = this.onChange.bind(this);
     this.copySecretToClipboard = this.copySecretToClipboard.bind(this);
 
-    // here we are: define your domain model
     this.InputService = t.struct({
       service: t.String,
       username: t.String,
       secret: t.String,
     });
-
-    // this.onImport = this.onImport.bind(this);
   }
 
-  componentWillMount() {
-    this.setState({
-      canAddService: false,
+  updateSecret(counterIncrement) {
+    const params = this.props.navigation.state.params;
+    const service = params.service;
+    const group = params.group;
+
+    const newService = new Service({
+      ...service,
+      counter: service.counter += counterIncrement,
     });
-  }
 
-  generateNewSecret() {
-    const service = this.props.navigation.state.params.service;
-    service.counter += 1;
-    this.props.dispatch(updateService(service));
-    this.props.navigation.setParams({ service });
-    this.setState(getStateFromProps(this.props.navigation.state.params)); // FIX This
-  }
-
-  revertSecret() {
-    const service = this.props.navigation.state.params.service;
-    service.counter -= 1;
-    this.props.navigation.setParams({ service });
-    this.props.dispatch(updateService(service));
-    this.setState(getStateFromProps(this.props.navigation.state.params)); // FIX This
+    this.props.dispatch(updateService(newService));
+    this.props.navigation.setParams({ service: newService });
+    this.setState({
+      value: {
+        ...this.state.value,
+        secret: newService.getSecret(group),
+      },
+    });
   }
 
   deleteService() {
@@ -85,8 +195,7 @@ class ServiceScreen extends React.Component {
   }
 
   copySecretToClipboard() {
-    const state = this.getState();
-    Clipboard.setString(state.value.secret);
+    Clipboard.setString(this.state.value.secret);
   }
 
   handlePress() {
@@ -112,10 +221,10 @@ class ServiceScreen extends React.Component {
           this.deleteService();
           break;
         case 0:
-          this.generateNewSecret();
+          this.updateSecret(1);
           break;
         case 1:
-          this.revertSecret();
+          this.updateSecret(-1);
           break;
         default:
           break;
@@ -125,28 +234,27 @@ class ServiceScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView
-        style={[Style.defaultBg, Style.container]}
-      >
+      <ScrollView style={[Style.defaultBg, Style.container]}>
         <Form
-          ref={(c) => { this.form = c; }}
           type={this.InputService}
           value={this.state.value}
-          onChange={this.onChange}
           options={{
             fields: {
               service: {
                 label: 'Site',
                 placeholder: 'example.com',
                 editable: false,
+                template: linkTemplate,
               },
               username: {
                 label: 'Username or email',
                 editable: false,
+                template: copyTemplate,
               },
               secret: {
                 label: 'Password',
                 editable: false,
+                template: copyTemplate,
               },
             },
           }}
